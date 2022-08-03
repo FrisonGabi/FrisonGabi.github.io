7c35afdf452bdd2395409ee1158a7bb38281c2fe
FrisonGabi <gfrisonbarbero@gmail.com>
2022-08-03 19:42:50 -0300

    un poco de mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,18 @@ import { Component,OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  menuActive:boolean = false
   
   ngOnInit(): void {
     //alert('Page in progress')
   }
 
-  
+  menu() {
+    this.menuActive = !this.menuActive
+    console.log(this.menuActive);
+    
+    
+  }
   
 }
